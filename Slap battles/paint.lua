844b9a2198e3873d30c219e7797a533f2cda3964
Ekanite-a <owo132187@gmail.com>
2025-10-30 17:13:46 +0800

    Update paint.lua

--- Slap battles/paint.lua
+++ Slap battles/paint.lua
@@ -1,5 +1,6 @@
 local workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 local char = game:GetService("Players").LocalPlayer.Character
 
 local npcs = {}
@@ -11,41 +12,23 @@ for _, v in ipairs(workspace:GetChildren()) do
     end
 end
 
-task.spawn(function()
-    local args
-    while task.wait() do
-        args = {"slap"}
-        ReplicatedStorage.Remotes.tool.use:FireServer(unpack(args))
-    end
-end)
-
-task.spawn(function()
-    local args
-    while task.wait() do
-        for _, v in ipairs(npcs) do
-            args = {"slap", {["Instance"] = v:FindFirstChild("Head")}}
-            ReplicatedStorage.Remotes.tool.hit:FireServer(unpack(args))
-        end
-    end
-end)
-
 local target
-task.spawn(function()
-    while task.wait() do
-        if target then
-            firetouchinterest(char:FindFirstChild("Hand", true), target.HumanoidRootPart, 0)
-            firetouchinterest(char:FindFirstChild("Hand", true), target.HumanoidRootPart, 1)
-            char.HumanoidRootPart.CFrame = target.HumanoidRootPart.CFrame
-
-            if target.Head.Transparency == 1 then target = nil end
-        end
-    end
-end)
-
-while task.wait() do
-    for _, v in ipairs(workspace:GetChildren()) do
-        if v.Name == "npc" and v.Head.Transparency == 0 and not v.HumanoidRootPart.Anchored then
+RunService.Heartbeat:Connect(function()
+    args = {"slap"}
+    ReplicatedStorage.Remotes.tool.use:FireServer(unpack(args))
+    for _, v in ipairs(npcs) do
+        args = {"slap", {["Instance"] = v:FindFirstChild("Head")}}
+        ReplicatedStorage.Remotes.tool.hit:FireServer(unpack(args))
+        
+        if not target and v.Head.Transparency == 0 and not v.HumanoidRootPart.Anchored then
             target = v
         end
     end
-end
+    if target then
+        firetouchinterest(char:FindFirstChild("Hand", true), target.HumanoidRootPart, 0)
+        firetouchinterest(char:FindFirstChild("Hand", true), target.HumanoidRootPart, 1)
+        char.HumanoidRootPart.CFrame = target.HumanoidRootPart.CFrame
+
+        if target.Head.Transparency == 1 then target = nil end
+    end
+end)
