c4de92cca6f66480b86cd2d95be91c5bab4b10cc
Ekanite-a <owo132187@gmail.com>
2025-11-26 21:14:28 +0800

    Add files via upload

--- Slap battles/conker.lua
+++ Slap battles/conker.lua
@@ -1,19 +1,18 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+task.wait(0.5)
+
 if game.PlaceId ~= 101113181694564 then 
     game:GetService("TeleportService"):Teleport(101113181694564)
 else
-    if not game:IsLoaded() then game.Loaded:Wait() end
-    task.wait(0.5)
-
     local workspace = game:GetService("Workspace")
     local ReplicatedStorage = game:GetService("ReplicatedStorage")
-    local player = game:GetService("Players").LocalPlayer
-    local hrp = player.Character.HumanoidRootPart
+    local hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
 
     ReplicatedStorage.Remotes.Dialogue.FinishedNPCDialogue:FireServer()
 
     task.wait(1)
     fireclickdetector(workspace.Map.Props.BasketCollection.Basket.ClickDetector)
-    task.wait(5)
+    task.wait(7.5)
     firetouchinterest(hrp, workspace.StartRoundPart, 0)
     firetouchinterest(hrp, workspace.StartRoundPart, 1)
 
@@ -25,16 +24,11 @@ else
             firetouchinterest(hrp, workspace.Conker, 1)
         end
 
-        if workspace.NPCs:FindFirstChild("squirrel") then
-            ReplicatedStorage.Remotes.tool.use:FireServer("slap")
-
-            local args = {
-                [1] = "slap",
-                [2] = {
-                    ["Instance"] = workspace.NPCs.squirrel.HumanoidRootPart
-                }
-            }
-            ReplicatedStorage.Remotes.tool.hit:FireServer(unpack(args))
+        if workspace.NPCs:FindFirstChild("squirrel") and workspace.NPCs.squirrel:FindFirstChild("HumanoidRootPart") then
+            ReplicatedStorage.Remotes.tool.hit:FireServer({
+                "slap",
+                {["Instance"] = workspace.NPCs.squirrel.HumanoidRootPart}
+            })
         end
         fireproximityprompt(workspace.Map.CoreAssets.Bowl.ProximityPrompt)
     end
@@ -48,35 +42,29 @@ task.wait(0.5)
 
 local workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local player = game:GetService("Players").LocalPlayer
-local hrp = player.Character.HumanoidRootPart
+local hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
 
 ReplicatedStorage.Remotes.Dialogue.FinishedNPCDialogue:FireServer()
 
 task.wait(1)
 fireclickdetector(workspace.Map.Props.BasketCollection.Basket.ClickDetector)
-task.wait(5)
+task.wait(7.5)
 firetouchinterest(hrp, workspace.StartRoundPart, 0)
 firetouchinterest(hrp, workspace.StartRoundPart, 1)
 
 while task.wait() do
     hrp.CFrame = CFrame.new(36, 4, 1.5)
-    
+        
     if workspace:FindFirstChild("Conker") then
         firetouchinterest(hrp, workspace.Conker, 0)
         firetouchinterest(hrp, workspace.Conker, 1)
     end
 
-    if workspace.NPCs:FindFirstChild("squirrel") then
-        ReplicatedStorage.Remotes.tool.use:FireServer("slap")
-
-        local args = {
-            [1] = "slap",
-            [2] = {
-                ["Instance"] = workspace.NPCs.squirrel.HumanoidRootPart
-            }
-        }
-        ReplicatedStorage.Remotes.tool.hit:FireServer(unpack(args))
+    if workspace.NPCs:FindFirstChild("squirrel") and workspace.NPCs.squirrel:FindFirstChild("HumanoidRootPart") then
+        ReplicatedStorage.Remotes.tool.hit:FireServer({
+            "slap",
+            {["Instance"] = workspace.NPCs.squirrel.HumanoidRootPart}
+        })
     end
     fireproximityprompt(workspace.Map.CoreAssets.Bowl.ProximityPrompt)
 end
