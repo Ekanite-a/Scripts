3c7403be6cdd23e5269faa735e4d0beb4a1ee0e2
Ekanite-a <owo132187@gmail.com>
2026-01-08 08:34:59 +0800

    Update plate.lua

--- Slap battles/plate.lua
+++ Slap battles/plate.lua
@@ -1,25 +1,19 @@
+local s = [[
 if not game:IsLoaded() then game.Loaded:Wait() end
 task.wait(0.5)
 
-local queue_on_teleport = queue_on_teleport or queueonteleport
-if queue_on_teleport and game.PlaceId ~= 106620300132058 then
-    queue_on_teleport(
-    [[if not game:IsLoaded() then game.Loaded:Wait() end
-    task.wait(0.5)
-    game.Workspace:WaitForChild("Obstacles"):Destroy()
-    while task.wait() do
-        firetouchinterest(game.Players.LocalPlayer.Character.Head, workspace["Glove Model"].Hand, 0)
-        firetouchinterest(game.Players.LocalPlayer.Character.Head, workspace["Glove Model"].Hand, 1)
-    end
-    ]])
+game.Workspace:WaitForChild("Obstacles"):Destroy()
+while task.wait() do
+    firetouchinterest(game.Players.LocalPlayer.Character.Head, workspace["Glove Model"].Hand, 0)
+    firetouchinterest(game.Players.LocalPlayer.Character.Head, workspace["Glove Model"].Hand, 1)
 end
+]]
+
+local qtp = queue_on_teleport or queueonteleport
 
 if game.PlaceId == 106620300132058 then
-    game.Workspace:WaitForChild("Obstacles"):Destroy()
-    while task.wait() do
-        firetouchinterest(game.Players.LocalPlayer.Character.Head, workspace["Glove Model"].Hand, 0)
-        firetouchinterest(game.Players.LocalPlayer.Character.Head, workspace["Glove Model"].Hand, 1)
-    end
+    loadstring(s)()
 else
+    qtp(s)
     game:GetService("TeleportService"):Teleport(106620300132058)
 end
